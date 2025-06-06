c8dbb1699ad807352b8fdda0c97f1779ff87bbc5
Lucas Yago <lucasyago63@gmail.com>
2025-06-06 15:48:25 -0300

    first commit

---
+++ src/test/java/br/com/onealura/TabelaFipe/TabelaFipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.onealura.TabelaFipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelaFipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
